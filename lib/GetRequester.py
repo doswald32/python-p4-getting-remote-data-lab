caa0789d1ab85c15db322cd35104924bc04f0bda
doswald32 <dan.oswald32@gmail.com>
2024-07-13 18:11:42 -0600

    completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,8 +6,23 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
+    @property
+    def url(self):
+        return self._url
+    
+    @url.setter
+    def url(self, url):
+        if isinstance(url, str):
+            self._url = url
+        else:
+            raise TypeError("URL must be a string.")
+
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        results = requests.get(self.url)
+        return results.json()
+
+
